6e0506b07572d03a52e0c8552c5b57060d55768c
Simen Andrè Vikestrand Skogum <sas@eika.no>
2025-03-02 11:02:01 +0100

    feat: trying to make it work

--- internal/controller/openbao_controller.go
+++ internal/controller/openbao_controller.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
@@ -41,16 +25,9 @@ type OpenBaoReconciler struct {
 // +kubebuilder:rbac:groups=instance.secrets.com,resources=openbaoes,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=instance.secrets.com,resources=openbaoes/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=instance.secrets.com,resources=openbaoes/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the OpenBao object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
 
 func (r *OpenBaoReconciler) ensureConfigMap(ctx context.Context, req ctrl.Request, openbao *storagev1alpha1.OpenBao) error {
 	cm := &corev1.ConfigMap{
@@ -67,7 +44,10 @@ func (r *OpenBaoReconciler) ensureConfigMap(ctx context.Context, req ctrl.Reques
 		cm.Data["config.json"] = openbao.Spec.Config
 		return nil
 	})
-	return err
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *OpenBaoReconciler) ensureStatefulSet(ctx context.Context, req ctrl.Request, openbao *storagev1alpha1.OpenBao) error {
@@ -129,7 +109,7 @@ func (r *OpenBaoReconciler) ensureStatefulSet(ctx context.Context, req ctrl.Requ
 						AccessModes: []corev1.PersistentVolumeAccessMode{
 							corev1.ReadWriteOnce,
 						},
-						Resources: corev1.VolumeResourceRequirements{
+						Resources: corev1.ResourceRequirements{
 							Requests: corev1.ResourceList{
 								corev1.ResourceStorage: resource.MustParse(openbao.Spec.StorageSize),
 							},
@@ -144,7 +124,10 @@ func (r *OpenBaoReconciler) ensureStatefulSet(ctx context.Context, req ctrl.Requ
 		sts.Spec.Replicas = &replicas
 		return nil
 	})
-	return err
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *OpenBaoReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
