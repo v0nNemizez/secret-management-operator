19d30477f6e1ca1dd6da18181c571e842b8b60c3
Simen Andrè Vikestrand Skogum <sas@eika.no>
2025-03-06 12:09:13 +0100

    fix: adding cluster reconsiler to main.go

--- cmd/main.go
+++ cmd/main.go
@@ -100,6 +100,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.ClusterReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Cluster")
+		os.Exit(1)
+	}
+
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
